e7d0514cd084c221d06c17fedbb69a91e3b79375
Cafe31 <github@malopaletou.pro>
2020-05-11 16:46:21 +0200

    Update files.py
    Adding some comments

--- methods/files.py
+++ methods/files.py
@@ -2,6 +2,7 @@
 
 import os, subprocess
 
+#add sshpass prefix if a password is filled
 def get_command_prefix(credentials, required_format="array"):
 	if required_format == "array":
 		if credentials['auth_method'] == "password":
@@ -12,6 +13,7 @@ def get_command_prefix(credentials, required_format="array"):
 			return "sshpass -p " + credentials['password']
 		return ""
 
+#check if a SSH server is available
 def verify_connection(credentials, command=["ls"]):
 	try:
 		subprocess.check_output(get_command_prefix(credentials) + ["ssh", credentials['username'] + "@" + credentials['server'], "-p", str(credentials['port'])] + command)
@@ -20,14 +22,17 @@ def verify_connection(credentials, command=["ls"]):
 		return False
 	return True
 
+#check if a path exists on an SSH server
 def directory_exists(credentials, path):
 	return verify_connection(credentials, ["ls", path])
 
+#compress a directory into a .tar.gz file
 def make_backup(backup_name, credentials, path, tmp_dir):
 	print(backup_name + ": Saving directory " + path)
 	os.system(get_command_prefix(credentials, "string") + " rsync -az -e 'ssh -p " + str(credentials['port']) + "' --ignore-existing " + credentials['username'] + "@" + credentials['server'] + ":" + path + " " + tmp_dir + backup_name)
 	print(backup_name + ": Directory " + path + " successfully saved")
 
+#upload a file using rsync
 def upload_file(credentials, target_path, backup_path):
 	print("Uploading final backup file...")
 
