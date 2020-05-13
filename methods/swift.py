fbc010078dc69e3ffc6c8c454ad3d259d57b9762
Cafe31 <github@malopaletou.pro>
2020-05-13 16:44:48 +0200

    Update swift.py
    Adding some corrections about target_path

--- methods/swift.py
+++ methods/swift.py
@@ -16,13 +16,13 @@ def upload_file(credentials, target_path, backup_path, backup_name):
 	with open(backup_path + backup_name, 'rb') as local:
 		conn.put_object(
 			credentials['container'],
-			backup_path + backup_name,
+			target_path + backup_name,
 			contents=local,
 			content_type='text/plain'
 		)
 
 	try:
-		resp_headers = conn.head_object(credentials['container'], backup_path + backup_name + "moi")
-		print('The object ' + backup_name + ' was successfully uploaded in ' + credentials['container'] + backup_path)
+		resp_headers = conn.head_object(credentials['container'], target_path + backup_name)
+		print('The object ' + backup_name + ' was successfully uploaded in ' + credentials['container'] + target_path)
 	except Exception as e:
 		print('An error occurred checking for the existence of the object in container')
